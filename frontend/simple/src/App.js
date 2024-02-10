86d58f61d048f7a4ce7298ec7a795a487144278e
shubhp nersc <shubhp@DOE7102450.local>
2024-02-10 10:48:29 -0800

    init

---
+++ frontend/simple/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          <h1>Simple React App</h1>
+          <p className="Context">
+            Hello I am Shubh! I will be your TA for this course. I have worked on various projects and have experience in web development, cloud computing, and machine learning.
+            Hope you have a great time in this course.
+          </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
