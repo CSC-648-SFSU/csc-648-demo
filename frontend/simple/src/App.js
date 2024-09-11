f40ba0d4a0de3a64cf98cb32ad0a43d7e566cbc0
shubh@DOE <shubhp@DOE7102450.local>
2024-09-11 10:06:49 -0700

    clean up

--- frontend/simple/src/App.js
+++ frontend/simple/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import profile from "./profile.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={profile} className="App-logo" alt="logo" />
           <h1>Simple React App</h1>
           <p className="Context">
-            Hello I am Shubh! I will be your TA for this course. I have worked on various projects and have experience in web development, cloud computing, and machine learning.
+            Hello I am Shubh! I will be your TA for this course. I have worked on various projects and have experience in web development and high performance computing.
             Hope you have a great time in this course.
           </p>
       </header>
